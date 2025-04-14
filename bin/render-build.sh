8177f1986b2f2b7ff4e42ac6518531438d10cf3d
Ronald Bisaso <bisasoronald313@gmail.com>
2025-04-14 17:15:24 +0300

    Make bin/rails executable

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,6 +7,9 @@ bundle install
 bin/rails assets:precompile
 bin/rails assets:clean
 
+chmod +x bin/rails
+
+
 # If you have a paid instance type, we recommend moving
 # database migrations like this one from the build command
 # to the pre-deploy command:
